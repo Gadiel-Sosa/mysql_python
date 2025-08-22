f1505223586fb6537ec1c6fb4b5dccc6485285c2
Gadiel-Sosa <gadielantoniososachan@gmail.com>
2025-08-22 12:34:45 -0600

    conexión a una bd con mysql.connector

---
+++ personas_db/primera_conexion.py
@@ -0,0 +1,26 @@
+# Importamos la conexión de mysql en python
+import mysql.connector
+# Creamos una variable donde almacenamos la conexion a la base de datos
+personas_db = mysql.connector.Connect(
+    host = 'localhost',
+    user = 'admin',
+    password = 'admin',
+    database = 'personas_db'
+)
+# Creamos al cursor que es el intermediario entre nosostros y la base de datos, nos ayuda con las consultas.
+mi_cursor = personas_db.cursor()
+# Ejecutamos una sentencia SELECT para mostrar el contenido de la base de datos
+mi_cursor.execute('SELECT * FROM personas')
+# Se almacenan los registros de la base de datos en una lista 
+resultados = mi_cursor.fetchall()
+# Iteramos la lsita de registros para imprimir cada uno de los elementos de la lista de manera presentable
+for resultado in resultados:
+    print(resultado)
+
+# Salida esperada usando el ciclo for:
+# (2, 'Sarha', 'Lee', 18)
+# (3, 'Marcos', 'Lee', 18)
+# etc.
+
+# Sin el ciclo for:
+#[(2, 'Sarha', 'Lee', 18), (3, 'Marcos', 'Lee', 18)]
